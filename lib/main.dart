2a6351111d6f90dfda67c73e08d3cd9a346695bf
‪Elsayed Mustafa‬‏ <110793510+El-Sayed-Mustafa@users.noreply.github.com>
2022-12-26 11:46:34 +0200

    Setting Up Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram/responsive/mobile_screen_layout.dart';
 import 'package:instagram/responsive/responsoive_layout_screen.dart';
 import 'package:instagram/responsive/web_screen_layout.dart';
 import 'package:instagram/utils/colors.dart';
 
-void main() {
+void main() async {
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: 'AIzaSyAGuT9g2pzhVSUvFQ0WABLnlFX4vSfqDH4',
+            appId: '1:566672712150:web:b1b3f557e08bab52294477',
+            messagingSenderId: '566672712150',
+            projectId: 'instagram-88755',
+            storageBucket: 'instagram-88755.appspot.com'));
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
@@ -20,7 +33,6 @@ class MyApp extends StatelessWidget {
             .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
         home: const ResponsiveLayout(
             webScreenLayout: WebScreenLayout(),
-            mobileScreenLayout: MobileScreenLayout())
-    );
+            mobileScreenLayout: MobileScreenLayout()));
   }
 }
